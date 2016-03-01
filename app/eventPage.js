78920910c3d3ecbcfa08ebc5e7c5aac3fef901cd
Timothy Poore <tpoore@ebsco.com>
2016-03-01 17:51:19 -0600

    Initial commit to git

---
+++ app/eventPage.js
@@ -0,0 +1,8 @@
+﻿chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action == "show") {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.pageAction.show(tabs[0].id);
+        });
+    }
+});
+
---
+++ app/rallyMailer_cs.js
@@ -0,0 +1,50 @@
+﻿chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.action == "getSelectedArtifacts") {
+        var activeArtifactOnly = false;
+        var selectedArtifacts = $('.x4-grid-row-selected');
+
+        var artifactObjects = selectedArtifacts.map(function(index, item) {
+            var itemRow = $(item);
+            var artifactLink = itemRow.find('td[class*=formattedid] a.formatted-id-link:first');
+            var artifactName = itemRow.find('td.name div:first');
+
+            var artifactLinkElement = artifactLink.get(0);
+
+            return {
+                artifactId: artifactLink.text(),
+                artifactHref: artifactLinkElement.href,
+                artifactName: artifactName.text()
+            };
+        });
+
+        var artifactsArray = $.makeArray(artifactObjects);
+
+        // When no artifacts have been checked then try to get the current artifact's detail page being shown
+        if (artifactsArray.length === 0) {
+            var artifactId = $('.formatted-id');
+            var artifactName = $('.detailFieldContainer input.simpleTextDetailField');
+
+            if (artifactId.length && artifactName.length) {
+                activeArtifactOnly = true;
+                artifactsArray.push({
+                    artifactId: artifactId.text(),
+                    artifactHref: document.location.href,
+                    artifactName: artifactName.val()
+                });
+            }
+        }
+
+        chrome.runtime.sendMessage({
+            action: "showSelectedArtifacts",
+            artifacts: artifactsArray,
+            activeArtifactOnly: activeArtifactOnly
+        });
+    }
+
+
+    if (request.action == "sendEmail") {
+        document.location.href = 'mailto:';
+    }
+});
+
+chrome.runtime.sendMessage({ action: "show" });
