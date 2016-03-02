1e4b387d4a5b1a446fe07412de177c4ed8ce9710
Timothy Poore <tpoore@ebsco.com>
2016-03-01 18:49:50 -0600

    Cleaned up selectedArtifacts code

--- app/rallyMailer_cs.js
+++ app/rallyMailer_cs.js
@@ -1,9 +1,32 @@
-﻿chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.action === 'getSelectedArtifacts') {
-        var activeArtifactOnly = false;
+﻿function showSelectedArtifacts(selectedArtifacts) {
+    chrome.runtime.sendMessage({
+        action: 'showSelectedArtifacts',
+        artifacts: selectedArtifacts,
+        activeArtifactOnly: false
+    });
+}
+
+function showActiveArtifact(activeArtifact) {
+    if (activeArtifact) {
+        chrome.runtime.sendMessage({
+            action: 'showSelectedArtifacts',
+            artifacts: [activeArtifact],
+            activeArtifactOnly: true
+        });
+    } else {
+        chrome.runtime.sendMessage({
+            action: 'showSelectedArtifacts',
+            artifacts: [],
+            activeArtifactOnly: false
+        });
+    }
+}
+
+var rallyArtifactHelper = {
+    getSelectedArtifacts: function() {
         var selectedArtifacts = $('.x4-grid-row-selected');
 
-        var artifactObjects = selectedArtifacts.map(function(index, item) {
+        var artifactObjects = selectedArtifacts.map(function (index, item) {
             var itemRow = $(item);
             var artifactLink = itemRow.find('td[class*=formattedid] a.formatted-id-link:first');
             var artifactName = itemRow.find('td.name div:first');
@@ -17,34 +40,42 @@
             };
         });
 
-        var artifactsArray = $.makeArray(artifactObjects);
+        return $.makeArray(artifactObjects);
+    },
+
+    // Gets the artifact details from the current Detail Page being shown
+    getDetailPageArtifact: function() {
+        var artifactId = $('.formatted-id');
+        var artifactName = $('.detailFieldContainer input.simpleTextDetailField');
+        if (artifactId.length && artifactName.length) {
+            return {
+                artifactId: artifactId.text(),
+                artifactHref: document.location.href,
+                artifactName: artifactName.val()
+            };
+        } else {
+            return null;
+        }
+    }
+};
+
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.action === 'getSelectedArtifacts') {
+        var selectedArtifacts = rallyArtifactHelper.getSelectedArtifacts();
 
         // When no artifacts have been checked then try to get the current artifact's detail page being shown
-        if (artifactsArray.length === 0) {
-            var artifactId = $('.formatted-id');
-            var artifactName = $('.detailFieldContainer input.simpleTextDetailField');
-
-            if (artifactId.length && artifactName.length) {
-                activeArtifactOnly = true;
-                artifactsArray.push({
-                    artifactId: artifactId.text(),
-                    artifactHref: document.location.href,
-                    artifactName: artifactName.val()
-                });
-            }
+        if (selectedArtifacts.length === 0) {
+            var activeArtifact = rallyArtifactHelper.getDetailPageArtifact();
+            showActiveArtifact(activeArtifact);
+        } else {
+            showSelectedArtifacts(selectedArtifacts);
         }
-
-        chrome.runtime.sendMessage({
-            action: 'showSelectedArtifacts',
-            artifacts: artifactsArray,
-            activeArtifactOnly: activeArtifactOnly
-        });
     }
-
 
     if (request.action === 'sendEmail') {
         document.location.href = 'mailto:';
     }
 });
 
-chrome.runtime.sendMessage({ action: 'show' });
+chrome.runtime.sendMessage({ action: 'showRallyMailerIcon' });
