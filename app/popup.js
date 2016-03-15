bf3d1e824bc9b7384fe5fcb8ccabf044f45dba7d
Timothy Poore <tpoore@ebsco.com>
2016-03-15 12:47:12 -0500

    Added basic support for selecting artifacts in legacy rally tree grid and browser tables

--- app/popup.js
+++ app/popup.js
@@ -81,17 +81,13 @@ var actions = {
 
 }
 
-$(function () {
-    /* Initialize */
-    actions.getSelectedArtifacts();
-
-    var activateActionButton = function (actionButton) {
-        var _action = actionButton;
-        _action
-            .velocity({
-                scale: 0
-            }, 0)
-            .velocity({
+function activateActionButton(actionButton) {
+    var action = actionButton;
+    action
+        .velocity({
+            scale: 0
+        }, 0)
+        .velocity({
             properties: {
                 scale: 1
             },
@@ -99,29 +95,71 @@ $(function () {
                 duration: 200,
                 display: 'block',
                 complete: function() {
-                    _action.tooltip({ delay: 50 });
+                    action.tooltip({ delay: 50 });
                 }
             }
         });
-    };
+}
 
-    var activateEmailAction = function() {
-        $('#copyToClipboard')
-            .velocity({
-                properties: {
-                    scale: 0,
-                },
-                options: {
-                    delay: 200,
-                    duration: 200,
-                    display: 'none',
-                    complete: function () {
-                        activateActionButton($('#sendEmail'));
-                    }
+function activateEmailAction() {
+    $('#copyToClipboard')
+        .velocity({
+            properties: {
+                scale: 0,
+            },
+            options: {
+                delay: 200,
+                duration: 200,
+                display: 'none',
+                complete: function() {
+                    activateActionButton($('#sendEmail'));
                 }
-            });
-    };
+            }
+        });
+}
 
+function showSelectedArtifacts(artifacts, activeArtifactOnly) {
+    /* When no artifacts are checked in rally show no artifacts message */
+    if (!artifacts || artifacts.length === 0) {
+        $('#noArtifactsSelected').show();
+        return;
+    }
+
+    var artifactsTableBody = $('#artifactsTableBody');
+    artifactsTableBody.empty();
+
+    artifacts.forEach(function (artifact) {
+        var artifactLink = '<a class="artifact-link" href="' + artifact.artifactHref + '">' + artifact.artifactId + '</a>';
+        var artifactRow = '<tr><td>' + artifactLink + '</td><td>' + artifact.artifactName + '</td></tr>';
+        artifactsTableBody.append(artifactRow);
+    });
+
+    if (activeArtifactOnly) {
+        $('.artifacts-header').text('Active Artifact');
+    } else {
+        $('.artifacts-header').text('Selected Artifacts');
+    }
+
+    // When artifact link is clicked copy the artifact as a visual studio commit comment
+    // Eg: [artifactId] artifactName
+    $('.artifact-link').click(function () {
+        var artifactId = $(this).text();
+        var artifactName = $(this).parent().next().text();
+        var artifactComment = '[' + artifactId + '] ' + artifactName;
+
+        var succeeded = actions.copyTextToClipboard(artifactComment);
+
+        if (succeeded) {
+            var artifactCommentToast = 'Copied ' + artifactId + ' to Clipboard';
+            Materialize.toast(artifactCommentToast, 3000);
+        }
+    });
+
+    $('#artifacts').show();
+    activateActionButton($('#copyToClipboard'));
+}
+
+function registerActionEvents() {
     $('#sendEmail').click(function () {
         if ($(this).hasClass('disabled')) return;
 
@@ -137,55 +175,18 @@ $(function () {
             $('#copiedBadge').velocity({ opacity: 1 }, { display: 'block' });
             activateEmailAction();
         }
-
     });
 
     chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-
         if (request.action === 'showSelectedArtifacts') {
-
-            /* When no artifacts are checked in rally show no artifacts message */
-            if (!request.artifacts || request.artifacts.length === 0) {
-                $('#noArtifactsSelected').show();
-                return;
-            }
-
-            var artifactsTableBody = $('#artifactsTableBody');
-            artifactsTableBody.empty();
-
-            request.artifacts.forEach(function (artifact) {
-                var artifactLink = '<a class="artifact-link" href="' + artifact.artifactHref + '">' + artifact.artifactId + '</a>';
-                var artifactRow = '<tr><td>' + artifactLink + '</td><td>' + artifact.artifactName + '</td></tr>';
-                artifactsTableBody.append(artifactRow);
-            });
-
-            if (request.activeArtifactOnly) {
-                $('.artifacts-header').text('Active Artifact');
-            } else {
-                $('.artifacts-header').text('Selected Artifacts');
-            }
-
-            // When artifact link is clicked copy the artifact as a visual studio commit comment
-            // Eg: [artifactId] artifactName
-            $('.artifact-link').click(function() {
-                var artifactId = $(this).text();
-                var artifactName = $(this).parent().next().text();
-                var artifactComment = '[' + artifactId + '] ' + artifactName;
-
-                var succeeded = actions.copyTextToClipboard(artifactComment);
-
-                if (succeeded) {
-                    var artifactCommentToast = 'Copied ' + artifactId + ' to Clipboard';
-                    Materialize.toast(artifactCommentToast, 3000);
-                }
-            });
-
-
-            $('#artifacts').show();
-            activateActionButton($('#copyToClipboard'));
+            showSelectedArtifacts(request.artifacts, request.activaeArtifactOnly);
         }
-
     });
+}
 
+$(function () {
+    /* Initialize */
+    actions.getSelectedArtifacts();
+    registerActionEvents();
 });
 
--- app/rallyMailer_cs.js
+++ app/rallyMailer_cs.js
@@ -22,25 +22,94 @@ function showActiveArtifact(activeArtifact) {
     }
 }
 
+// Creates artifact objects from passed in collection of artifact elements
+function mapElementsToArtifactObjects(artifactElements, artifactLinkSelector, artifactNameSelector) {
+    return artifactElements.map(function (index, item) {
+        var artifactElement = $(item);
+        var artifactLink = artifactElement.find(artifactLinkSelector);
+        var artifactName = artifactElement.find(artifactNameSelector);
+
+        var artifactLinkElement = artifactLink.get(0);
+
+        return {
+            artifactId: artifactLink.text(),
+            artifactHref: artifactLinkElement.href,
+            artifactName: artifactName.text()
+        };
+    });
+}
+
+// Gets selected artifacts from any part of rally using the newer rally tree grid with built in checkboxes
+function getSelectedTreeGridArtifacts() {
+    var selectedArtifacts = $('.x4-tree-view.grid-view-bulk-edit .x4-grid-row-selected');
+    var artifactLinkSelector = 'td[class*=formattedid] a.formatted-id-link:first';
+    var artifactNameSelector = 'td.name div:first';
+    return $.makeArray(mapElementsToArtifactObjects(selectedArtifacts, artifactLinkSelector, artifactNameSelector));
+}
+
+// Gets selected artifacts for search grid in rally (https://rally1.rallydev.com/#/search)
+function getSelectedSearchArtifacts() {
+    var selectedArtifacts = $('.rally-mailer-search-select:checked').parentsUntil('tbody', 'tr');
+    var artifactLinkSelector = 'td[class*=formattedid] a.formatted-id-link:first';
+    var artifactNameSelector = 'td.name div:first';
+
+    // Note: reverse is being used here because parentsUntil gets collection bottom up instead of top down
+    return $.makeArray(mapElementsToArtifactObjects(selectedArtifacts, artifactLinkSelector, artifactNameSelector)).reverse();
+}
+
+// Gets selected artifacts for browser table in defect (https://rally1.rallydev.com/#/defects) and task (https://rally1.rallydev.com/#/tasks) views
+function getSelectedBrowserArtifacts() {
+    var selectedArtifacts = $('.rally-mailer-browser-select:checked').parentsUntil('tbody', 'tr');
+    var artifactLinkSelector = 'td.cn_formattedid0 a:first';
+    var artifactNameSelector = 'td.cn_name0 a:first';
+
+    // Note: reverse is being used here because parentsUntil gets collection bottom up instead of top down
+    return $.makeArray(mapElementsToArtifactObjects(selectedArtifacts, artifactLinkSelector, artifactNameSelector)).reverse();
+}
+
+// Creates the ui component that will be injected into the dom
+function createArtifactUiSelector(selectorClass) {
+    var selector = $('<input type="checkbox" />').addClass(selectorClass);
+    return selector;
+}
+
+// Gets all injectable artifact elements
+function getInjectableArtifactElements(artifactElementSelector) {
+    var allInjectableElements = $(artifactElementSelector);
+
+    // Filter out injectable elements that already contain injected selector
+    var injectableElements = allInjectableElements.filter(function (index, element) {
+        return $(element).find('.rally-mailer-select').first().length === 0;
+    });
+
+    return injectableElements;
+}
+
+// Adds artifact ui selector for search grid in rally (https://rally1.rallydev.com/#/search)
+function injectSearchArtifactSelectors() {
+    var artifactUiSelector = createArtifactUiSelector('rally-mailer-select rally-mailer-search-select');
+    var artifactElementSelector = '.x4-tree-view:not(.grid-view-bulk-edit) .x4-grid-data-row td:first-child';
+    var injectableElements = getInjectableArtifactElements(artifactElementSelector);
+    injectableElements.prepend(artifactUiSelector);
+}
+
+// Adds artifact ui selector for browser table in defect (https://rally1.rallydev.com/#/defects) and task (https://rally1.rallydev.com/#/tasks) views
+function injectBrowserArtifactSelectors() {
+    var artifactUiSelector = createArtifactUiSelector('rally-mailer-select rally-mailer-browser-select');
+    var artifactElementSelector = '.browser > tbody > tr > td:first-child';
+    var injectableElements = getInjectableArtifactElements(artifactElementSelector);
+    injectableElements.prepend(artifactUiSelector);
+}
+
 var rallyArtifactHelper = {
-    getSelectedArtifacts: function() {
-        var selectedArtifacts = $('.x4-grid-row-selected');
 
-        var artifactObjects = selectedArtifacts.map(function (index, item) {
-            var itemRow = $(item);
-            var artifactLink = itemRow.find('td[class*=formattedid] a.formatted-id-link:first');
-            var artifactName = itemRow.find('td.name div:first');
+    // Gets all artifacts that selected in the dom
+    getAllSelectedArtifacts: function () {
+        var treeGridArtifacts = getSelectedTreeGridArtifacts();
+        var searchArtifacts = getSelectedSearchArtifacts();
+        var browserArtifacts = getSelectedBrowserArtifacts();
 
-            var artifactLinkElement = artifactLink.get(0);
-
-            return {
-                artifactId: artifactLink.text(),
-                artifactHref: artifactLinkElement.href,
-                artifactName: artifactName.text()
-            };
-        });
-
-        return $.makeArray(artifactObjects);
+        return [].concat.apply([], [treeGridArtifacts, searchArtifacts, browserArtifacts]);
     },
 
     // Gets the artifact details from the current Detail Page being shown
@@ -56,13 +125,32 @@ var rallyArtifactHelper = {
         } else {
             return null;
         }
+    },
+
+    // Adds ui selectors to rally artifact views that don't contain a selection mechanism
+    injectArtifactUiSelectors: function () {
+        injectSearchArtifactSelectors();
+        injectBrowserArtifactSelectors();
     }
 };
+
+function poll() {
+    setTimeout(checkForLegacyGrid, 3000);
+}
+
+function checkForLegacyGrid() {
+    rallyArtifactHelper.injectArtifactUiSelectors();
+    poll();
+}
+
+$(function () {
+    poll();
+});
 
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.action === 'getSelectedArtifacts') {
-        var selectedArtifacts = rallyArtifactHelper.getSelectedArtifacts();
+        var selectedArtifacts = rallyArtifactHelper.getAllSelectedArtifacts();
 
         // When no artifacts have been checked then try to get the current artifact's detail page being shown
         if (selectedArtifacts.length === 0) {
